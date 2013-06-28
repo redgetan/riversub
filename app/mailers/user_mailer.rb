4af06c6e88847a1223e3ad1b98bdd2a3dd6a9b4d
Reginald Tan <redge.tan@gmail.com>
2013-06-27 21:43:00 -0400

    changed email url to riversub.cc

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@zeroplay.net"
+  default from: "noreply@riversub.cc"
 
   def welcome_email(user)
     @user = user
-    @url  = "http://www.zeroplay.net/"
+    @url  = "http://www.riversub.cc/"
     mail(:to => user.email, :subject => "Welcome to River Subtitles")
   end
 end
