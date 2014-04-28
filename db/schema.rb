fec5a6f91ff51bd77153c13d2027b491a979019c
Hassan Hassan <hassan@raise5.com>
2014-04-27 22:47:31 -0400

    these are my changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20140330220356) do
 
-  create_table "conversations", :force => true do |t|
-    t.string   "subject",    :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-  end
-
   create_table "identities", :force => true do |t|
     t.string   "uid"
     t.string   "provider"
@@ -26,40 +20,6 @@ ActiveRecord::Schema.define(:version => 20140330220356) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  create_table "notifications", :force => true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              :default => ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                :default => false
-    t.datetime "updated_at",                              :null => false
-    t.datetime "created_at",                              :null => false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-    t.boolean  "global",               :default => false
-    t.datetime "expires"
-  end
-
-  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
-
-  create_table "receipts", :force => true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                                  :null => false
-    t.boolean  "is_read",                       :default => false
-    t.boolean  "trashed",                       :default => false
-    t.boolean  "deleted",                       :default => false
-    t.string   "mailbox_type",    :limit => 25
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-  end
-
-  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "repositories", :force => true do |t|
     t.integer  "video_id",   :null => false
@@ -71,17 +31,6 @@ ActiveRecord::Schema.define(:version => 20140330220356) do
   create_table "settings", :force => true do |t|
     t.string "key"
     t.string "value"
-  end
-
-  create_table "subtitle_requests", :force => true do |t|
-    t.integer  "video_id",                                    :null => false
-    t.integer  "user_id",                                     :null => false
-    t.string   "from_language"
-    t.string   "to_language"
-    t.decimal  "price",         :precision => 8, :scale => 2
-    t.string   "status"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
   end
 
   create_table "subtitles", :force => true do |t|
@@ -97,14 +46,6 @@ ActiveRecord::Schema.define(:version => 20140330220356) do
     t.float    "start_time"
     t.float    "end_time"
     t.integer  "subtitle_id"
-  end
-
-  create_table "translations", :force => true do |t|
-    t.integer  "subtitle_id", :null => false
-    t.string   "language"
-    t.text     "text"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
