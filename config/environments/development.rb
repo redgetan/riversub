db38d79c7f78ea5efa5ce49cd3d1f77f48d3ed94
Reginald Tan <redge.tan@gmail.com>
2014-05-28 22:22:13 -0400

    same email config for dev/production and add ssl => true to make it work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,9 @@ config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
-    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :port      => 465, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
+    :ssl => true,
     :user_name => "hassan@yasub.com",
     :password  => "UfFUzJ-HJHxIkqHlmji8MA", # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ River::Application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 465, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
+    :ssl => true,
     :user_name => "hassan@yasub.com",
     :password  => "UfFUzJ-HJHxIkqHlmji8MA", # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
