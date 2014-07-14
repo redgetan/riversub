88bd91450b99394a575d563d6ddd8945ce75ad8c
Reginald Tan <redge.tan@gmail.com>
2014-07-14 19:39:35 -0400

    show only published videos in user profile page when not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
+
+    @repositories = if user_signed_in? && current_user == @user
+                      @user.repositories # all
+                    else
+                      @user.repositories.published # if not logged in, show only published ones
+                    end
   end
 end
