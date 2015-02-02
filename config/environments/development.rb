8dc8bd776fc4d27cdbb8b66b2b42d8788b362419
Reginald Tan <redge.tan@gmail.com>
2015-02-02 16:08:45 -0500

    use gmail smtp via redge@yasub.com for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,19 +16,17 @@ River::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 465, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :ssl => true,
-    :user_name => "hassan@yasub.com",
-    :password  => "UfFUzJ-HJHxIkqHlmji8MA", # SMTP password is any valid API key
-    :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'yasub.com', # your domain to identify your server when connecting
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "yasub.com",
+    :user_name            => 'redge@yasub.com',
+    :password             => 'iwtets90',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
-
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
