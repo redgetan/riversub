dc4129105e22e922b78894f15f7280fac14cb985
Reginald Tan <redge.tan@gmail.com>
2015-02-01 21:19:58 -0500

    title change only enabled for templateeditor

--- app/assets/javascripts/ui/template_editor.js
+++ app/assets/javascripts/ui/template_editor.js
@@ -48,6 +48,88 @@ river.ui.TemplateEditor = river.ui.Editor.extend({
       this.replayTrackAndEdit(this.currentTrack);
     }
   },
+  
+  enableTitleChange: function() {
+    $(".repo_label_container").find("#repo_label").hide();
+
+    var title_input = "<div class='' title='click to edit'>" +
+                        "<input type='text' class='repo_title_input' placeholder='Enter Title'>" +
+                      "</div>";
+
+    $(".repo_label_container").append(title_input);
+
+    this.$titleInputContainer = $(".repo_label_container").find(".repo_title_input_container");
+    this.$titleInput = $(".repo_label_container").find(".repo_title_input");
+    this.$titleInputHandle = $(".repo_label_container").find(".title_input_handle");
+
+    if (repo.title) {
+      this.$titleInput.val(repo.title);
+    }
+
+    this.$titleInput.on("keyup", this.onTitleInputKeyup.bind(this));
+    this.$titleInput.on("blur", this.onTitleInputBlur.bind(this));
+    this.$titleInputContainer.on("mouseenter", this.onTitleInputMouseEnter.bind(this));
+    this.$titleInputContainer.on("mouseleave", this.onTitleInputMouseLeave.bind(this));
+    this.$titleInputHandle.on("click", this.onTitleInputHandleClick.bind(this));
+    this.$previewBtn.on("click", this.onPreviewBtnClick.bind(this));
+  },
+
+  onTitleInputMouseEnter: function() {
+  },
+
+  onTitleInputMouseLeave: function() {
+  },
+
+  onTitleInputKeyup: function(event) {
+    // enter key
+    if (event.which == 13 ) {
+      this.$titleInput.blur();
+    }
+  },
+
+  onTitleInputHandleClick: function(event) {
+    this.$titleInput.focus();
+  },
+
+  onTitleInputBlur: function() {
+    this.saveRepoTitle();
+  },
+
+  saveRepoTitle: function(callback) {
+    var repoTitle = this.$titleInput.val();
+
+    this.saveNotify();
+
+    $.ajax({
+      url: this.repo.update_title_url,
+      type: "POST",
+      dataType: "json",
+      data: { repo_title: repoTitle },
+      success: function(data) {
+        repo.title = repoTitle;
+        this.clearStatusBar();
+        if (typeof callback !== "undefined") {
+          callback();
+        }
+      }.bind(this),
+      error: function(data) {
+        this.clearStatusBar();
+        alert("Unable to save Title. We would look into this shortly.");
+        throw data.responseText;
+      }.bind(this)
+    });
+  },
+
+  onPreviewBtnClick: function(event) {
+    if (this.$titleInput.val().length > 0) {
+      this.saveRepoTitle(function(){
+        this.previewRepo(event);
+      }.bind(this));
+    } else {
+      this.previewRepo(event);
+    }
+  },
+
 
 
 })
