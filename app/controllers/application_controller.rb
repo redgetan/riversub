2b6a6b3df1afd9d2532516bfd1b23047372c93a4
Reginald Tan <redge.tan@gmail.com>
2015-02-03 15:50:51 -0500

    redirect to proper page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   #before_filter :authenticate_user!
-
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+
+  def store_previous_url
+    session[:previous_url] = request.referer
   end
 
   def render_404
@@ -17,7 +20,20 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-    user_url(user)
+    user_action = if query = URI.parse(request.referer).query 
+                    CGI.parse(query)["user_action"][0]
+                  else
+                    nil
+                  end
+
+    case user_action
+    when "upvote_a_comment","upvote_a_subtitle","add_a_language","upload_a_subtitle_file"
+      session[:previous_url]
+    when "subtitle_a_video"
+      videos_new_url
+    else
+      user_url(user)
+    end
   end
 
 end
