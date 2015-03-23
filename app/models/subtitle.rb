00b5b9628b92fa2def2d662a3bc97653d1878fe5
Reginald Tan <redge.tan@gmail.com>
2015-03-23 19:35:36 -0400

    make sure no newlines are present in subtitle

--- app/models/subtitle.rb
+++ app/models/subtitle.rb
@@ -6,12 +6,18 @@ class Subtitle < ActiveRecord::Base
 
   has_one    :timing
 
+  before_save :strip_crlf_text
+
   def serialize
     {
       :id => self.id,
       :text => self.text,
       :parent_text => self.parent_text.to_s
     }
+  end
+
+  def strip_crlf_text
+    self.text.gsub("\n"," ")
   end
 
 end
