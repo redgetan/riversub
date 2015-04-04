799e598fe892504c2f64f98af71f7a69bfd629a6
Reginald Tan <redge.tan@gmail.com>
2015-04-04 17:31:05 -0400

    yt sync in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :fetch_youtube_owner_caption, :only => [:show]
   def show
     @user = User.find_by_username(params[:username])
 
@@ -7,5 +8,8 @@ class UsersController < ApplicationController
                     else
                       @user.repositories.published # if not logged in, show only published ones
                     end
+
+
+      doc.css("entry")[0].css("content")[0].attributes["lang"].value
   end
 end
