355148a88c276e3f956cd3e65bfd1ff5efde0c02
Reginald Tan <redge.tan@gmail.com>
2015-07-16 17:04:34 -0400

    related vids should repo belonging to same user + use sql for randomness

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -64,11 +64,17 @@ class Repository < ActiveRecord::Base
   GUIDED_WALKTHROUGH_YOUTUBE_URL = "http://www.youtube.com/watch?v=6tNTcZOpZ7c"
   ANONYMOUS_USERNAME = "default"
 
-  def self.related(repo)
+  def self.related(repo, num_of_entries = 10)
     if repo.group.try(:short_name) == "jpweekly"
-      self.where(group_id: repo.group_id).where(language: "en").shuffle.take(10)
+      self.where(group_id: repo.group_id).where(language: "en").order("RAND()").limit(num_of_entries)
+    elsif repo.group.present?
+      self.where(group_id: repo.group_id).order("RAND()").limit(num_of_entries)
+    elsif repo.user.present?
+      user_repos = self.where(user_id: repo.user_id)
+      other_repos = self.order("RAND()").limit(num_of_entries - user_repos.count)
+      user_repos + other_repos
     else
-      self.where(group_id: repo.group_id).shuffle.take(10)
+      self.order("RAND()").limit(num_of_entries)
     end
   end
 
