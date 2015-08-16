09550a1dede3912f5cefe7a3ef2f49566e9dd98c
Reginald Tan <redge.tan@gmail.com>
2015-08-15 22:01:44 -0400

    show non-jap subs in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < ApplicationController
     @group = Group.find_by_short_name("jpweekly")
     @activities = PublicActivity::Activity.order("created_at DESC").limit(6)
     @repos = Repository.includes({:timings => :subtitle}, :video, :user)
-                       .where(language: "en").published.recent.limit(5)
+                       .where("language <> 'ja'").published.recent.limit(5)
 
     respond_to :html
   end
 
   def community_translations
     @repos = Repository.includes({:timings => :subtitle}, :video, :user)
-                       .where(language: "en").published.recent.page params[:page]
+                       .where("language <> 'ja'").published.recent.page params[:page]
   end
 
   def features
