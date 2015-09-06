f5e8739f8b2809615fa1d5a112a0c26c3235d7aa
Reginald Tan <redge.tan@gmail.com>
2015-09-06 15:48:42 -0400

    fix image opengraph of groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -139,7 +139,8 @@ class Group < ActiveRecord::Base
   end
 
   def thumbnail_url_hq
-    avatar_url
+    root_url_without_trailing_slash = root_url[0..-2]
+    root_url_without_trailing_slash + avatar_url
   end
 
   def share_text
