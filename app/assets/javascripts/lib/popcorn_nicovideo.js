33a0171e267559ea56dafab97258fbd49e9ff2f8
Reginald Tan <redge.tan@gmail.com>
2015-10-01 15:32:19 -0400

    add popcorn integration to nico

---
+++ app/assets/javascripts/lib/popcorn_nicovideo.js
@@ -0,0 +1,506 @@
+(function( Popcorn, window, document ) {
+
+  var
+
+  CURRENT_TIME_MONITOR_MS = 16,
+  EMPTY_STRING = "",
+
+  // Example: http://www.nicovideo.jp/watch/sm26803766
+  regexNicovideo = /.*nicovideo.jp\/watch\/(.*)/,
+
+  ABS = Math.abs,
+
+  function HTMLNicoVideoElement( id ) {
+
+    var self = this,
+      parent = typeof id === "string" ? document.querySelector( id ) : id,
+      elem = document.createElement( "div" ),
+      impl = {
+        src: EMPTY_STRING,
+        networkState: self.NETWORK_EMPTY,
+        readyState: self.HAVE_NOTHING,
+        seeking: false,
+        autoplay: EMPTY_STRING,
+        preload: EMPTY_STRING,
+        controls: false,
+        loop: false,
+        poster: EMPTY_STRING,
+        volume: 1,
+        muted: false,
+        currentTime: 0,
+        duration: NaN,
+        ended: false,
+        paused: true,
+        error: null
+      },
+      playerReady = false,
+      mediaReady = false,
+      loopedPlay = false,
+      player,
+      playerPaused = true,
+      bufferedInterval,
+      lastLoadedFraction = 0,
+      currentTimeInterval,
+      timeUpdateInterval,
+      firstPlay = true,
+      lastPlayerTime;
+
+    // Namespace all events we'll produce
+    self._eventNamespace = Popcorn.guid( "HTMLNicoVideoElement::" );
+
+    self.parentNode = parent;
+
+    // Mark this as YouTube
+    self._util.type = "Nico";
+
+    function onNicoPlayerReady() {
+      // dispatch loaded metadata  
+
+      playerReady = true;
+      player = document.external_nico_0; // the nicoplayer flash object
+
+
+      impl.duration = player.ext_getTotalTime();
+      impl.readyState = self.HAVE_METADATA;
+      self.dispatchEvent( "loadedmetadata" );
+      currentTimeInterval = setInterval( monitorCurrentTime,
+                                         CURRENT_TIME_MONITOR_MS );
+      
+      self.dispatchEvent( "loadeddata" );
+
+      impl.readyState = self.HAVE_FUTURE_DATA;
+      self.dispatchEvent( "canplay" );
+
+      // We can't easily determine canplaythrough, but will send anyway.
+      impl.readyState = self.HAVE_ENOUGH_DATA;
+      self.dispatchEvent( "canplaythrough" );
+
+      self.dispatchEvent( "loadstart" );
+    }
+
+    function getDuration() {
+      return document.external_nico_0.ext_getTotalTime();
+    }
+
+    function onNicoPlayerStatus( object, event ) {
+      switch( event ) {
+
+        // ended
+        case "end":
+          onEnded();
+          // Seek back to the start of the video to reset the player,
+          player.ext_setPlayheadTime( 0 );
+          break;
+
+        // playing
+        case "playing":
+          if( firstPlay ) {
+            // fake ready event
+            firstPlay = false;
+            bufferedInterval = setInterval( monitorBuffered, 50 );
+          }
+
+          impl.paused = false;
+          onPlay();
+          break;
+
+        // paused
+        case "paused":
+          onPause();
+          break;
+
+        // paused
+        case "seeking":
+          onSeeking();
+          onSeeked();
+          break;
+      }
+    }
+
+    function destroyPlayer() {
+      if( !( playerReady && player ) ) {
+        return;
+      }
+      clearInterval( currentTimeInterval );
+      clearInterval( bufferedInterval );
+      player.ext_play(false);
+
+      parent.removeChild( elem );
+      elem = document.createElement( "div" );
+    }
+
+    function changeSrc( aSrc ) {
+      if( !self._canPlaySrc( aSrc ) ) {
+        impl.error = {
+          name: "MediaError",
+          message: "Media Source Not Supported",
+          code: MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED
+        };
+        self.dispatchEvent( "error" );
+        return;
+      }
+
+      impl.src = aSrc;
+
+      if( playerReady ) {
+        destroyPlayer();
+      }
+
+      parent.appendChild( elem );
+
+      // Get ID out of nicovideo url
+      aSrc = regexNicovideo.exec( aSrc )[ 1 ];
+
+      var thumbwatch = "<script type='text/javascript' src='http://ext.nicovideo.jp/thumb_watch/" + aSrc + "'></script>";
+      elem.innerHTML = thumbwatch;
+    }
+
+    function monitorCurrentTime() {
+      var playerTime = player.ext_setPlayheadTime();
+      var playing = player.ext_getStatus();
+
+      if ( !impl.seeking ) {
+
+        // var oldCurrentTime = impl.currentTime;
+
+        // making nico player emit time at 60fps - http://stackoverflow.com/a/24514978/803865
+        var playerTimeHasNotChanged = lastPlayerTime == playerTime;
+        if (playing == 1 && playerTimeHasNotChanged) {
+          impl.currentTime += CURRENT_TIME_MONITOR_MS/1000;
+        } else {
+          impl.currentTime = playerTime;
+        }
+
+        // if (ABS( oldCurrentTime - playerTime ) > 0) {
+        //   onTimeUpdate();
+        // }
+
+        if ( ABS( impl.currentTime - playerTime ) > CURRENT_TIME_MONITOR_MS ) {
+          // no need
+          onSeeking();
+          onSeeked();
+        }
+      } else if ( ABS( playerTime - impl.currentTime ) < 1 ) {
+        onSeeked();
+      }
+
+      lastPlayerTime = playerTime;
+    }
+
+    function monitorBuffered() {
+      var fraction = player.ext_getLoadedRatio();
+
+      if ( lastLoadedFraction !== fraction ) {
+        lastLoadedFraction = fraction;
+
+        onProgress();
+
+        if ( fraction >= 1 ) {
+          clearInterval( bufferedInterval );
+        }
+      }
+    }
+
+    function getCurrentTime() {
+      return impl.currentTime;
+    }
+
+    function changeCurrentTime( aTime ) {
+      onSeeking();
+      player.ext_setPlayheadTime( aTime );
+      impl.currentTime = player.ext_getPlayheadTime();
+    }
+
+    function onTimeUpdate() {
+      self.dispatchEvent( "timeupdate" );
+    }
+
+    function onSeeking() {
+      impl.seeking = true;
+      self.dispatchEvent( "seeking" );
+    }
+
+    function onSeeked() {
+      impl.ended = false;
+      impl.seeking = false;
+      self.dispatchEvent( "timeupdate" );
+      self.dispatchEvent( "seeked" );
+      self.dispatchEvent( "canplay" );
+      self.dispatchEvent( "canplaythrough" );
+    }
+
+    function onPlay() {
+
+      if( impl.ended ) {
+        changeCurrentTime( 0 );
+        impl.ended = false;
+      }
+      clearInterval(timeUpdateInterval);
+      timeUpdateInterval = setInterval( onTimeUpdate,
+                                        self._util.TIMEUPDATE_MS );
+      impl.paused = false;
+
+      if( playerPaused ) {
+        playerPaused = false;
+
+        // Only 1 play when video.loop=true
+        if ( ( impl.loop && !loopedPlay ) || !impl.loop ) {
+          loopedPlay = true;
+          self.dispatchEvent( "play" );
+        }
+        self.dispatchEvent( "playing" );
+      }
+    }
+
+    function onProgress() {
+      self.dispatchEvent( "progress" );
+    }
+
+    self.play = function() {
+      impl.paused = false;
+      player.ext_play(true);
+    };
+
+    function onPause() {
+      impl.paused = true;
+      if ( !playerPaused ) {
+        playerPaused = true;
+        clearInterval( timeUpdateInterval );
+        self.dispatchEvent( "pause" );
+      }
+    }
+
+    self.pause = function() {
+      impl.paused = true;
+      player.ext_play(false);
+    };
+
+    function onEnded() {
+      if( impl.loop ) {
+        changeCurrentTime( 0 );
+        self.play();
+      } else {
+        impl.ended = true;
+        onPause();
+        self.dispatchEvent( "timeupdate" );
+        self.dispatchEvent( "ended" );
+      }
+    }
+
+    function setVolume( aValue ) {
+      impl.volume = aValue;
+      player.ext_setVolume( impl.volume * 100 );
+      self.dispatchEvent( "volumechange" );
+    }
+
+    function getVolume() {
+      // Nico has ext_getVolume(), but for sync access we use impl.volume
+      return impl.volume;
+    }
+
+    function setMuted( aValue ) {
+      impl.muted = aValue;
+      setVolume(0);
+      self.dispatchEvent( "volumechange" );
+    }
+
+    function getMuted() {
+      // YouTube has isMuted(), but for sync access we use impl.muted
+      return impl.muted;
+    }
+
+    Object.defineProperties( self, {
+
+      src: {
+        get: function() {
+          return impl.src;
+        },
+        set: function( aSrc ) {
+          if( aSrc && aSrc !== impl.src ) {
+            changeSrc( aSrc );
+          }
+        }
+      },
+
+      autoplay: {
+        get: function() {
+          return impl.autoplay;
+        },
+        set: function( aValue ) {
+          impl.autoplay = self._util.isAttributeSet( aValue );
+        }
+      },
+
+      loop: {
+        get: function() {
+          return impl.loop;
+        },
+        set: function( aValue ) {
+          impl.loop = self._util.isAttributeSet( aValue );
+        }
+      },
+
+      width: {
+        get: function() {
+          return self.parentNode.offsetWidth;
+        }
+      },
+
+      height: {
+        get: function() {
+          return self.parentNode.offsetHeight;
+        }
+      },
+
+      currentTime: {
+        get: function() {
+          return getCurrentTime();
+        },
+        set: function( aValue ) {
+          changeCurrentTime( aValue );
+        }
+      },
+
+      duration: {
+        get: function() {
+          return getDuration();
+        }
+      },
+
+      ended: {
+        get: function() {
+          return impl.ended;
+        }
+      },
+
+      paused: {
+        get: function() {
+          return impl.paused;
+        }
+      },
+
+      seeking: {
+        get: function() {
+          return impl.seeking;
+        }
+      },
+
+      readyState: {
+        get: function() {
+          return impl.readyState;
+        }
+      },
+
+      networkState: {
+        get: function() {
+          return impl.networkState;
+        }
+      },
+
+      playerObject: {
+        get: function () {
+          return player;
+        }
+      },
+
+      volume: {
+        get: function() {
+          // Remap from HTML5's 0-1 to YouTube's 0-100 range
+          var volume = getVolume();
+          return volume / 100;
+        },
+        set: function( aValue ) {
+          if( aValue < 0 || aValue > 1 ) {
+            throw "Volume value must be between 0.0 and 1.0";
+          }
+
+          setVolume( aValue );
+        }
+      },
+
+      muted: {
+        get: function() {
+          return getMuted();
+        },
+        set: function( aValue ) {
+          setMuted( self._util.isAttributeSet( aValue ) );
+        }
+      },
+
+      error: {
+        get: function() {
+          return impl.error;
+        }
+      },
+
+      buffered: {
+        get: function () {
+          var timeRanges = {
+            start: function( index ) {
+              if ( index === 0 ) {
+                return 0;
+              }
+
+              //throw fake DOMException/INDEX_SIZE_ERR
+              throw "INDEX_SIZE_ERR: DOM Exception 1";
+            },
+            end: function( index ) {
+              var duration;
+              if ( index === 0 ) {
+                duration = getDuration();
+                if ( !duration ) {
+                  return 0;
+                }
+
+                return duration * player.getVideoLoadedFraction();
+              }
+
+              //throw fake DOMException/INDEX_SIZE_ERR
+              throw "INDEX_SIZE_ERR: DOM Exception 1";
+            }
+          };
+
+          Object.defineProperties( timeRanges, {
+            length: {
+              get: function() {
+                return 1;
+              }
+            }
+          });
+
+          return timeRanges;
+        }
+      }
+    });
+  }
+
+  HTMLNicoVideoElement.prototype = new Popcorn._MediaElementProto();
+  HTMLNicoVideoElement.prototype.constructor = HTMLNicoVideoElement;
+
+  // Helper for identifying URLs we know how to play.
+  HTMLNicoVideoElement.prototype._canPlaySrc = function( url ) {
+    return (regexNicovideo).test( url ) ?
+      "probably" :
+      EMPTY_STRING;
+  };
+
+  Popcorn.HTMLNicoVideoElement = function( id ) {
+    return new HTMLNicoVideoElement( id );
+  };
+  Popcorn.HTMLNicoVideoElement._canPlaySrc = HTMLNicoVideoElement.prototype._canPlaySrc;
+
+  Popcorn.player( "nicovideo", {
+    _canPlayType: function( nodeName, url ) {
+      return ( typeof url === "string" &&
+               Popcorn.HTMLNicoVideoElement._canPlaySrc( url ) );
+    }
+  });
+
+  Popcorn.nicovideo = function( container, url, options ) {
+    var media = Popcorn.HTMLNicoVideoElement( container ),
+        popcorn = Popcorn( media, options );
+
+    return popcorn;
+  };
+
+}( Popcorn, window, document ));
